67269d51393e8e711198eef67a1da47debb9b9a8
Akash Singh <36884150+jordan9800@users.noreply.github.com>
2024-06-29 11:47:12 +0530

    Add seeders, run pint

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // check if table types is empty
+        if (DB::table('types')->count() == 0) {
+
+            DB::table('types')->insert([
+
+                [
+                    'name' => 'Marketing',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'Invoices',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'System',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+
+            ]);
+
+        }
+    }
+}
